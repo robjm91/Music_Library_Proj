19513370de584645313f8a8437e3aff35da3b122
Robert J Magaña <robjmagana@gmail.com>
2020-11-18 19:06:18 -0800

    Styled page with some CSS. Also deleted line comments in index.js

--- index.js
+++ index.js
@@ -31,10 +31,6 @@ function getMusicLibrary() {
 }
 
 
-
-
-
-
 $(document).ready(function () {
     $("h1").css("color", "red");
     $("h1").css("margin", "auto");
@@ -43,30 +39,3 @@ $(document).ready(function () {
 getMusicLibrary();
 });
 
-
-
-
-
-/*
-$(selector).(action)  selector finds HTML elements, and action is performed on the elements.
-
-jQuery - Selectors
-
-$("#some-id");     <-- <ul id="some-id"> 
-$(".some-class");  <-- <ul class="some-class">
-$("ul li");             <-- <li>
-
-Examples:
-
-$("ul li");
-
-$("ul li").filter("#coffee");   <-- finds all coffee
-
-$("ul").find("#coffee");   <-- finds 1st instance of coffee
-
-<ul id="drinks">
-    <li id="coffee">Espresso</li>
-    <li id="coffee">Cold brew</li>
-</ul>
-
-*/
