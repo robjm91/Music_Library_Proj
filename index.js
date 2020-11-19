1f0e16e5e42ccc234e398060deafa2788488bc6b
Robert J Magaña <robjmagana@gmail.com>
2020-11-18 18:48:54 -0800

    Added table to provide place for music library API data
    Also added postman collection of GET ID and GET ALL

--- index.js
+++ index.js
@@ -1,36 +1,47 @@
-function getMusicLibrary(){
-    $(document).ready(function() {
+
+function getMusicLibrary() {
+    $(document).ready(function () {
         $.ajax({
-            url: "http://www.devcodecampmusiclibrary.com/api/music#",
-            type: "GET",
-            dataType: "json",
-            success: function(){
-                $(".musicLibrary").html("");
-            }
-        })
+                url: "http://www.devcodecampmusiclibrary.com/api/music#",
+                type: "GET",
+                dataType: "json",
+                success: function () {
+                    $(".musicLibrary").html("");
+                }
+            })
+            .then(function (data) {
+                $.each(data, function (index, value) {
+                    $("#musicLibrary").append( 
+                        "<tr>" +
+                            "<td>" + value.id + "</td>" +
+                            "<td>" + value.title + "</td>" +
+                            "<td>" + value.album + "</td>" +
+                            "<td>" + value.artist + "</td>" +
+                            "<td>" + value.genre + "</td>" +
+                            "<td>" + value.releaseDate + "</td>" +
+                        "</tr>"
+
+                    )
+
+                })
+
+            })
     })
 
 }
-$(document).ready(function() {
+
+getMusicLibrary();
+
+
+
+
+
+$(document).ready(function () {
     $("h1").css("color", "red");
     $("h1").css("margin", "auto");
     $("h1").css("text-align", "center");
 });
 
-/*
-$.ajax({
-    url: "http://www.devcodecampmusiclibrary.com/api/music",
-    dataType: "json",
-    type: "get",
-    success: function (data, textStatus, jQXhr) {
-        //do something with response data (data parameter)
-    },
-    error: function (jQXhr, textStatus, errorThrown) {
-        // if request fails, the errorThrown parameter will contain details of the error
-    },
-});
-
-*/
 
 
 
